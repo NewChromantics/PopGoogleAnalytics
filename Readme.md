c63b0285c0f9ba46a1479d27afbe354885ac2158
Graham Reeves <graham@grahamreeves.com>
2021-06-19 16:59:52 +0100

    Now getting a response from GA. But events not showing up. Adding validation request

--- Readme.md
+++ Readme.md
@@ -2,3 +2,6 @@ Installation
 ===============================
 Clone or submodule this repository into `YourProject/Plugins/PopGoogleAnalytics` `(gr: does the name need to explicitly be this?)`
 
+measurement ID G-H854WHM16Z
+stream id 2652615313
+Secret1 _qzH82vMRJ2jU2WoGEBqiQ
--- Source/PopGoogleAnalytics/Private/PopGoogleAnalyticsBPLibrary.cpp
+++ Source/PopGoogleAnalytics/Private/PopGoogleAnalyticsBPLibrary.cpp
@@ -3,6 +3,7 @@
 #include "PopGoogleAnalyticsBPLibrary.h"
 #include "PopGoogleAnalytics.h"
 #include "HttpModule.h"
+#include "Interfaces/IHttpResponse.h"
 #include "Containers/UnrealString.h"
 
 #define LOCTEXT_NAMESPACE "PopGoogleAnalytics"
@@ -11,7 +12,7 @@
 class GoogleAnalyticsRequest_t //: public UObject
 {
 public:
-	GoogleAnalyticsRequest_t();
+	GoogleAnalyticsRequest_t(FString JsonString,bool ValidationRequest);
 	
 protected:
 	void		OnRequestCompleted(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
@@ -24,17 +25,32 @@ public:
 //	put these in the library?
 TArray<TSharedRef<GoogleAnalyticsRequest_t>> gRequests;
 
+//	ids and keys from google analytics
+//	see live feedback in the debug view
+//	https://analytics.google.com/analytics/web/#/m/p275755571/debugview/overview
+const FString MeasurementId = "G-H854WHM16Z";
+//const FString StreamId = "2652615313";
+const FString ApiSecret = "_qzH82vMRJ2jU2WoGEBqiQ";
+const FString MeasurementProtocolUrl = "https://www.google-analytics.com/mp/collect";
+//	this is a validation url which sends back debug if the response is malformed, the normal url doesn't report errors
+const FString ValidateMeasurementProtocolUrl = "https://www.google-analytics.com/debug/mp/collect";
 
-
-GoogleAnalyticsRequest_t::GoogleAnalyticsRequest_t()
+GoogleAnalyticsRequest_t::GoogleAnalyticsRequest_t(FString JsonString,bool ValidationRequest)
 {
 	auto Request = FHttpModule::Get().CreateRequest();
 	mRequest = Request;
-	//mRequest->SetVerb("POST");
-	//mRequest->SetHeader("Content-Type", "application/json");
-	FString Url = "https://four.tours/gitlastcommit";
-	mRequest->SetURL(*FString::Printf(TEXT("%s"), *Url));
-	//mRequest->SetContentAsString(JsonString);
+	
+	//	https://developers.google.com/analytics/devguides/collection/protocol/ga4/sending-events
+	mRequest->SetVerb("POST");
+	mRequest->SetHeader("Content-Type", "application/json");
+	FString Url = ValidationRequest ? ValidateMeasurementProtocolUrl : MeasurementProtocolUrl;
+	mRequest->SetURL(*FString::Printf( TEXT("%s?measurement_id=%s&api_secret=%s"), *Url, *ApiSecret, *MeasurementId ) );
+	
+	//	required json params
+	//	.client_id=string	https://developers.google.com/gtagjs/reference/api#get_mp_example
+	//	.events=[]
+	//	https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#payload_query_parameters
+	mRequest->SetContentAsString(JsonString);
 	
 	auto Callback = [this](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 	{
@@ -43,12 +59,24 @@ GoogleAnalyticsRequest_t::GoogleAnalyticsRequest_t()
 	mRequest->OnProcessRequestComplete().BindLambda(Callback);
 	//mRequest->OnProcessRequestComplete().BindUObject(this, &GoogleAnalyticsRequest_t::OnRequestCompleted);
 
-	mRequest->ProcessRequest();
+	if ( !mRequest->ProcessRequest() )
+	{
+		UE_LOG( LogTemp, Error, TEXT( "GoogleAnalytics ProcessRequest() failed") );
+	}
 }
 	
 void GoogleAnalyticsRequest_t::OnRequestCompleted(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
-	UE_LOG( LogTemp, Log, TEXT( "%s" ), __PRETTY_FUNCTION__ );
+	auto ResponseCode = Response ? Response->GetResponseCode() : 0;
+	FString ResponseBody = Response ? Response->GetContentAsString() : "";
+	//if ( bWasSuccessful )
+	if ( ResponseCode == 200 )
+	{
+		UE_LOG( LogTemp, Log, TEXT( "GoogleAnalytics report completed. (%d), body:%s"), ResponseCode, *ResponseBody );
+		return;
+	}
+	
+	UE_LOG( LogTemp, Error, TEXT( "GoogleAnalytics report failed; response code %d; %s"), ResponseCode, *ResponseBody );
 }
 
 
@@ -69,7 +97,12 @@ void UPopGoogleAnalyticsBPLibrary::PopGoogleAnalyticsPostEvent(FString Json)
 	UE_LOG( LogTemp, Log, TEXT( "PopGoogleAnalyticsPostEvent" ) );
 	try
 	{
-		TSharedRef<GoogleAnalyticsRequest_t> NewRequest( new GoogleAnalyticsRequest_t() );
+		//	.client_id=string	https://developers.google.com/gtagjs/reference/api#get_mp_example
+		//	.events=[]
+		FString TestJson = "{ \"client_id\":\"UEClient.Test\", \"events\":[{\"name\":\"TestEvent1\"}] }";
+	
+		bool Validate = true;
+		TSharedRef<GoogleAnalyticsRequest_t> NewRequest( new GoogleAnalyticsRequest_t(TestJson,Validate) );
 		gRequests.Add(NewRequest);
 	}
 	catch(std::exception& e)
